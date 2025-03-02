1fec375d8d9a48eec0a9a9d461dbbe726a357a79
AfrinaDipti <diptiafrina24@gmail.com>
2025-03-03 00:38:20 +0600

    Initiate ui_style.py

---
+++ ui_styles.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Dict
+
+@dataclass
+class Colors:
+    pass
+
+
+@dataclass
+class Styles:
+    # Card styles
+    pass
+
+def configure_styles(style):
+    """Configure ttk styles with our custom theme"""
+    pass
+
+CATEGORIES = [
+    "Food",
+    "Transportation",
+    "Utilities",
+    "Entertainment",
+    "Shopping",
+    "Healthcare",
+    "Education",
+    "Other"
+] 
